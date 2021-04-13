efcaacb06a17ff74bdb6f1c399f09b34e6c8f1d5
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-04-13 20:26:19 +0100

    new changes

--- week-04/day-03/23-Ins_Local-Storage-Objects/assets/js/script.js
+++ week-04/day-03/23-Ins_Local-Storage-Objects/assets/js/script.js
@@ -4,24 +4,23 @@ var comment = document.getElementById("msg");
 var saveButton = document.getElementById("save");
 var savedName = document.getElementById("saved-name");
 
-saveButton.addEventListener("click", function(event) {
-event.preventDefault();
+saveButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-var studentGrade = {
-  student: student.value,
-  grade: grade.value,
-  comment: comment.value.trim()
-};
+  var studentGrade = {
+    student: student.value,
+    grade: grade.value,
+    comment: comment.value.trim(),
+  };
 
-localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-renderMessage();
-
+  localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
+  renderMessage();
 });
 
 function renderMessage() {
   var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
   if (lastGrade !== null) {
-    document.querySelector(".message").textContent = lastGrade.student + 
-    " received a/an " + lastGrade.grade
+    document.querySelector(".message").textContent =
+      lastGrade.student + " received a/an " + lastGrade.grade;
   }
 }
